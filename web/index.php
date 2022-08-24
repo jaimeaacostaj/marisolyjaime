1396315606c40c31acb1617d0682bce94c890014
jaaj <jaime@calixe.com>
2022-08-24 00:38:27 -0500

    hola

--- web/index.php
+++ web/index.php
@@ -1,27 +1,2 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+echo "Hola mundo";
